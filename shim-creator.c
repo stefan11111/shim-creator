4f2e1f8b8e351f0b23f87d829ff7d07e801a7b0a
stefan11111 <stefan11111@shitposting.expert>
2025-01-02 14:14:27 +0200

    use buffered i/o

--- shim-creator.c
+++ shim-creator.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
 #define STR_MAX 4096
-#define _STR(x) #x
-#define STR(x) _STR(x)
+#define STR_(x) #x
+#define STR(x) STR_(x)
 
 int main(int argc, char **argv)
 {
     if (argc != 4) {
-        fprintf(stderr, "Usage: %s [input] [output.c] [output.h] \n", argv[0]);
+        fprintf(stderr, "Usage: %s [input] [output.c] [output.h]\n", argv[0]);
         return 0;
     }
 
@@ -15,30 +16,25 @@ int main(int argc, char **argv)
     FILE *g = fopen(argv[2], "w");
     FILE *h = fopen(argv[3], "w");
 
-
     for (;;) {
-        char c;
-        if (fscanf(f, "%c", &c) == EOF) {
+        char str[STR_MAX + 1];
+        if (fscanf(f, "%" STR(STR_MAX) "s", str) == EOF) {
             return 0;
         }
 
-        if (c != ' ') {
+        if (str[0] != 'T' || str[1] != 0) {
             continue;
         }
-        if (fscanf(f, "%c", &c) == EOF) {
-            return 0;
+
+        if (fscanf(f, "%" STR(STR_MAX) "s", str) == EOF) {
+            return 0; /* should never happen */
         }
-        if (c != 'T') {
-            continue;
+
+        char *p = strchr(str, '@');
+        if (p) {
+            *p = 0;
         }
-        if (fscanf(f, "%c", &c) == EOF) {
-            return 0;
-        }
-        if (c != ' ') {
-            continue;
-        }
-        char str[STR_MAX + 1];
-        fscanf(f, "%" STR(STR_MAX) "s", str);
+
         fprintf(g, "void* %s () { return 0; }\n", str);
         fprintf(h, "void* %s ();\n", str);
     }
